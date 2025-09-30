0a059ce4a57b6eddd0aaecfee37d8aa72eeb5a14
esteban <estmontenegro1@gmail.com>
2025-09-29 23:26:09 -0500

    creacion modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Example } from './example/example';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Example
+
+    }
+];
